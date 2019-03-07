47977fd584509220a1bc7cd472676f2fa8a90229
zheng.huang <zheng.huang@hzjo.com>
2019-03-07 16:49:27 +0800

    test4-->1

--- src/main/java/com/example/james/creative/patterns/single/Singleton.java
+++ src/main/java/com/example/james/creative/patterns/single/Singleton.java
@@ -17,7 +17,7 @@ public class Singleton {
         if (null == instance) {
             instance = new Singleton();
         }
-        System.out.println("这里是test4");
+        System.out.println("这里是test5");
         return instance;
     }
 }
